38896b0b91d918aad4215b1f49184a830eaac987
Guilherme Batista de Souza <146041046+guilhermebatista7@users.noreply.github.com>
2024-01-31 13:30:33 -0300

    js da lista de tarefas

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 function adicionaTarefa() {
     let input = document.getElementById('tarefa').value;
     let lista = document.getElementById('tarefas_adicionadas');
-    lista.innerHTML += '<div class="conteudo"><p><input type="checkbox"/>'+input+'<button id=deleta onclick="deletar()">Deletar</button></p></div>';
+    let novoItem = document.createElement('div');
+    novoItem.className = 'conteudo';
+    novoItem.innerHTML = '<p id="item_lista"><input type="checkbox"/>'+input+'<button onclick="deletar(this)" style="margin-left: 7px;">Deletar</button></p>';
+    lista.appendChild(novoItem);
+}
+
+function deletar(elementoDeleta) {
+    let conteudo = elementoDeleta.parentNode.parentNode;
+    conteudo.parentNode.removeChild(conteudo);
 }
