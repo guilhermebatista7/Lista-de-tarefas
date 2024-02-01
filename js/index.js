2f1ac942b32154b3ffc4118d2014b6f6055c1d6a
Guilherme Batista de Souza <146041046+guilhermebatista7@users.noreply.github.com>
2024-02-01 00:09:01 -0300

    ajustes no projeto da lista

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 function adicionaTarefa() {
     let input = document.getElementById('tarefa').value;
-    let lista = document.getElementById('tarefas_adicionadas');
-    let novoItem = document.createElement('div');
-    novoItem.className = 'conteudo';
-    novoItem.innerHTML = '<p id="item_lista"><input type="checkbox"/>'+input+'<button onclick="deletar(this)" style="margin-left: 7px;">Deletar</button></p>';
-    lista.appendChild(novoItem);
+    if(!input){
+        alert('Digite uma tarefa')
+    }else{ 
+
+        let lista = document.getElementById('tarefas_adicionadas');
+        let novoItem = document.createElement('div');
+        novoItem.className = 'conteudo';
+        novoItem.innerHTML = '<p id="item_lista"><input type="checkbox"/>'+input+'<button onclick="deletar(this)" id="deleta">Deletar</button></p>';
+        lista.appendChild(novoItem);
+    }
 }
 
 function deletar(elementoDeleta) {
